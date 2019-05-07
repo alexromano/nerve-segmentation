2700d05184525c855b5f0289c5937f2f2b5dd9f8
Alex Romano <alexromano@apple.com>
2019-05-06 18:34:32 -0700

    fixed out of order bug

--- submission.py
+++ submission.py
@@ -4,44 +4,59 @@ from PIL import Image
 import os
 import csv
 import cv2
+import tensorflow as tf
 from tensorflow.keras.models import Model, load_model
 from train import preprocess
 
-original_width = 420
-original_height = 580
+original_width = 580
+original_height = 420
+test_dir = "ultrasound-nerve-segmentation/test"
 
 def get_test_images():
     print("Getting test images..")
-    files = os.listdir("ultrasound-nerve-segmentation/test")
-    test_imgs = []
-    for f in files[:10]:
-        im = Image.open("ultrasound-nerve-segmentation/test/"+f)
+    files = os.listdir(test_dir)
+    num_files = len(files)
+    
+    test_imgs = np.zeros((num_files, 128, 128), dtype='float32')
+    img_ids = np.zeros((num_files, ), dtype='uint32')
+
+    for i in range(len(files)):
+        img_id = int(files[i].split('.')[0])
+        im = Image.open(test_dir+"/"+files[i])
         im_arr = preprocess(im)
-        test_imgs.append(im_arr)
+        img_ids[i] = img_id
+        test_imgs[i] = im_arr
+
     mean = np.mean(test_imgs)
     std = np.std(test_imgs)
     test_imgs -= mean
     test_imgs /= std
-    return np.expand_dims(np.array(test_imgs, dtype='float32'), axis=3)
+    return np.expand_dims(test_imgs, axis=3), img_ids
 
-def get_predictions(images):
+def get_predictions(images, ids):
     print("Loading model..")
-    model = load_model('model_weights.hd5', compile=False)
+    model = load_model('model_weights_overfit.hd5', compile=False)
     img_masks = model.predict(images, batch_size=256, verbose=1)
-    
+
+    # crucial to sort predictions in order for the kaggle evaluation
+    argsort = np.argsort(ids)
+    ids = ids[argsort]
+    img_masks = img_masks[argsort]
+
     print("Running predictions")
     img_masks_sized = np.zeros((img_masks.shape[0], original_height, original_width)) 
     for i in range(img_masks.shape[0]):
         img_masks_sized[i] = cv2.resize(img_masks[i], (original_width, original_height))
-
+    np.save("predictions.npy", img_masks_sized)
+    print(len(img_masks_sized))
     print("Saving predictions")
     with open(str(dt.now())+'.csv', 'w') as f:
         writer = csv.writer(f, delimiter=',')
-        writer.writerow(["image", "pixels"])
+        writer.writerow(["img", "pixels"])
         for i, mask in enumerate(img_masks_sized):
-            mask[mask >= 0.5] = 1
-            mask[mask < 0.5] = 0
-            writer.writerow([str(i + 1), run_length_enc(mask)])
+            mask = (mask > 0.5).astype('uint8')
+            print(ids[i])
+            writer.writerow([str(ids[i]), run_length_enc(mask)])
 
 # run length encoding
 def run_length_enc(label):
@@ -59,8 +74,8 @@ def run_length_enc(label):
     return ' '.join([str(r) for r in res])
 
 def submission():
-    test_images = get_test_images()
-    get_predictions(test_images)
+    test_images, img_ids = get_test_images()
+    get_predictions(test_images, img_ids)
 
 # img, rle
 # 1, 1 1 4 10
